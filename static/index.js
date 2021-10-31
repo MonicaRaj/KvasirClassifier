f3aefcc84746f0ea5188e539bd5e443afbb6530a
MonicaRaj <monicageo.annauniv@gmail.com>
2021-10-30 23:39:21 -0400

    Completed the application and works as expected

--- static/index.js
+++ static/index.js
@@ -1,8 +1,24 @@
 // https://github.com/bhattbhavesh91/tf-js-example-1
 // https://machinelearningknowledge.ai/learn-image-classification-with-tensorflow-js-using-mobilenet-model-web-app
 // https://towardsdatascience.com/deploying-an-image-classifier-using-javascript-84da1480b3a4
-
-
+var img;
+$(document).ready(function() {
+     $('#loading').hide();
+     $('select').on('change', function() {
+        document.getElementById("imageDiv").style.display = "block";
+        document.getElementById("selected-image").src = this.value;
+        $("#prediction-list-div").empty();
+        $('#loading').hide();
+        img = new Image()
+        img.src=document.getElementById("selected-image").src;
+        img.crossOrigin = "anonymous";
+        
+    });
+    $('#predict-button').on('click', function() {
+        getPred(img);
+    });
+    
+});
 CLASSES = {0: "Ampulla of vater",
            1: "Angiectasia",
            2: "Blood - fresh",
@@ -19,20 +35,25 @@ CLASSES = {0: "Ampulla of vater",
            13: "Ulcer"}
 
 function PreviewImage() {
+    $('#loading').show();
+    document.getElementById("imageDiv").style.display = "block";
+    document.getElementById("field_value").value = "0";    
     var oFReader = new FileReader();
     oFReader.readAsDataURL(document.getElementById("image-selector").files[0]);
     oFReader.onload = function (oFREvent) {
         document.getElementById("selected-image").src = oFREvent.target.result;
-        $("#prediction-list").empty();
+        $("#prediction-list-div").empty();
+        $('#loading').hide();
+        img = $("#selected-image").get(0);
     };
 };
 
-async function getPred(){
+async function getPred(img){
+    $('#loading').show();
 	// Read the image and perform preprocessing
-    // const img = new Image()
-    // img.src='https://image.shutterstock.com/image-vector/vector-illustration-unused-match-stick-260nw-1662505090.jpg';
-    // img.crossOrigin = "anonymous";
-    let image = $("#selected-image").get(0);
+   
+    let image = img;
+    console.log(image)
 	let tensorImg = tf.browser.fromPixels(image).resizeNearestNeighbor([32, 32]).toFloat().div(tf.scalar(255)).expandDims();
     const predictions = await model.predict(tensorImg).data();
 	
@@ -47,16 +68,21 @@ async function getPred(){
         return b.probability - a.probability;
     }).slice(0, 5);
 	
-	// Display the results
-    top5.forEach(function (p) {
-        const probability = (p.probability * 100).toFixed(2);
-        $("#prediction-list").append("<li>" + p.className + " : " + probability + " %</li>")
-    });
+
+    colorList = ["bg-success", "bg-info", "bg-warning", "bg", "bg-danger"]
+   
+    for(i=0;i<top5.length;i++){
+        const probability = (top5[i].probability * 100).toFixed(2);
+        $("#prediction-list-div").append("<div class='row'><div class='col-4'>" + top5[i].className + "</div><div class='progress' style='width: 50%;'><div class='progress-bar " + colorList[i] + "' role='progressbar' style='width: " + probability + "%'>" + probability + "%</div></div></div><br>")
+        if(i==top5.length-1){
+            $('#loading').hide();
+        }
+    }
 }
 
 async function loadModel() {
   model = undefined;
-  model = await tf.loadLayersModel("cnn_model_e200/model.json");
+  model = await tf.loadLayersModel("https://raw.githubusercontent.com/MonicaRaj/KvasirClassifier/main/static/cnn_model_e200/model.json");
   console.log("model loaded")
 }
 loadModel();
